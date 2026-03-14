7ad835f63fa1b995050aa99bc5eff823d722e483
Elian <elian_fl@hotmail.com>
2026-03-14 18:23:20 +0100

    deleted duplicated motor

--- src/utils/ui/sportName.ts
+++ src/utils/ui/sportName.ts
@@ -3,7 +3,6 @@ const SPORT_NAME_ES: Record<string, string> = {
   soccer: 'Futbol',
   basketball: 'Baloncesto',
   tennis: 'Tenis',
-  motor: 'Motor',
   motorsport: 'Motor',
   cycling: 'Ciclismo',
 };
